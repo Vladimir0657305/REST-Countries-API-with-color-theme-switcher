27cd916b724913369ff35576a71c49ca69701621
Vladimir0657305 <vv0657305@gmail.com>
2022-12-29 14:43:25 +0200

    changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import {useState, useEffect} from 'react';
+import { Container } from "./Container";
+import { IoMoon, IoMoonOutline } from 'react-icons/io5'
+
+const HeaderEl = styled.header`
+    box-shadow: var(--shadow);
+    background-color: var(--colors-ui-base);
+`;
+
+const Wrapper = styled.div``;
+
+const Title = styled.a.attrs({
+    href: "/",
+})``;
+
+const ModeSwitcher = styled.div``;
+
+
+
+export const Header = () => {
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme])
+
+    return(
+        <HeaderEl>
+            <Container>
+                <Wrapper>
+                    <Title>Where isi the world?</Title>
+                    <ModeSwitcher onClick={toggleTheme}>
+                        <IoMoon/> Light Theme
+                    </ModeSwitcher>
+                </Wrapper>
+            </Container>
+        </HeaderEl>
+    );
+}
