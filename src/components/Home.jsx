490e70044022d9bd117cc1fd372c9ae932995636
Vladimir0657305 <vv0657305@gmail.com>
2022-12-31 17:35:27 +0200

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,30 @@
+import './Home.scss';
+
 export default function Home({ countries, setCountries }){
+    const { v4: uuidv4 } = require('uuid');
     // console.log(countries[0]['flags']['png']);
     console.log(countries[0]);
     return(
-        <>
-            <div className="block-home">
+        <div className='home'>
+            <div className='homeList'>
                 {
                 // countries.map(c => (c['flags']['png']))
                 countries.map(c => 
-                <img src={c['flags']['png']} />
-                    // <p key={c.name}>{c.name}</p>
+                    <div className="block-home">
+                        <img key={uuidv4()} src={c['flags']['png']} />
+                        <br/>
+                        <span key={uuidv4()}><b>{c.name}</b></span>
+                        <br />
+                        <span key={uuidv4()}><b>Population: </b>{c.population}</span>
+                        <br />
+                        <span key={uuidv4()}><b>Region: </b>{c.region}</span>
+                        <br />
+                        <span key={uuidv4()}><b>Capital: </b>{c.capital}</span>
+                    </div>
+                    
                 )
                 }
             </div>
-        </>
+        </div>
     );
 }
