668b82736428961035166cdffe82cc6fcd50ce09
Vladimir0657305 <vv0657305@gmail.com>
2023-01-03 10:32:56 +0200

    completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,22 @@ import { Link } from 'react-router-dom';
 let themeLocalStorage = '';
 
 export default function Header() {
-    const [theme, setTheme] = useState('Dark');
+    const [theme, setTheme] = useState(localStorage.getItem('themeLocalStorage') ? localStorage.getItem('themeLocalStorage') : 'Dark');
     // const [themeLocalStorage, setThemeLocalStorage] = useState('');
-    
-
-    
 
     useEffect(() => {
+                let temp = localStorage.getItem('themeLocalStorage');
+                console.log('temp=',temp);
+                // console.log(localStorage.getItem('themeLocalStorage'));
+                setTheme(temp);
+                console.log('theme=', theme);
+                document.body.setAttribute('data-theme', temp);
+                // setThemeLocalStorage(temp);
+                // document.body.setAttribute('data-theme', temp);
+            },[]);
+
+    useEffect(() => {
+        console.log(theme);
         document.body.setAttribute('data-theme', theme);
         localStorage.setItem('themeLocalStorage',theme);
     }, [theme])
@@ -25,14 +34,8 @@ export default function Header() {
             )
 
     }
-    useEffect(() => {
-            // const temp = localStorage.getItem('themeLocalStorage');
-            console.log(localStorage.getItem('themeLocalStorage'));
-            setTheme(localStorage.getItem('themeLocalStorage'));
-            document.body.setAttribute('data-theme', localStorage.getItem('themeLocalStorage'));
-            // setThemeLocalStorage(temp);
-            // document.body.setAttribute('data-theme', temp);
-        },[])
+    
+
     return (
         <div className='container'>
             <div className='menu'>
